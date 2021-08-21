df30f74891dc86f484381be45e1fedee50cee09a
Evgeniy Karmyskov <67991583+Karmyshkov@users.noreply.github.com>
2021-08-21 23:22:01 +0300

    Fixed bug

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 $('.header__links').on('click', 'a', function(){
-	$('.header__links a').removeClass('header__link_type_active');
-	$(this).addClass('header__link_type_active');
+	$('.header__links a').removeClass('header__link_active');
+	$(this).addClass('header__link_active');
 });
